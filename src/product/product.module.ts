98b688c8469a1ca56cfa17d1783fe8690ffd5c4b
Darshan Chobarkar <dchobarkar@gmail.com>
2024-02-23 18:02:33 +0530

    connect with mysql

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductController],
 })
 export class ProductModule {}
